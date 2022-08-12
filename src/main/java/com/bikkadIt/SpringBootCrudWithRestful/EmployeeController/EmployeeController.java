4eaf7417dd46fe80c7cf0bfa01ea95a7a016c257
swarna1993 <swarna.krisanya@gmail.com>
2022-08-13 04:18:03 +0530

    SBCWR-5:create controller class

---
+++ src/main/java/com/bikkadIt/SpringBootCrudWithRestful/EmployeeController/EmployeeController.java
@@ -0,0 +1,8 @@
+package com.bikkadIt.SpringBootCrudWithRestful.EmployeeController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+}
