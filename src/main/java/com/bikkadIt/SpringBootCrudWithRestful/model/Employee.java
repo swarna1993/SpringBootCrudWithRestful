37702306eeb151e6c4206bedc2339a4938e02bcd
swarna1993 <swarna.krisanya@gmail.com>
2022-08-13 04:39:24 +0530

    SBCWR-2:create Entity class

--- src/main/java/com/bikkadIt/SpringBootCrudWithRestful/model/Employee.java
+++ src/main/java/com/bikkadIt/SpringBootCrudWithRestful/model/Employee.java
@@ -1,7 +1,60 @@
 package com.bikkadIt.SpringBootCrudWithRestful.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int empId;
+	
+	private String empName;
+	
+	private String empAddress;
+	
+	private double empSalary;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpAddress() {
+		return empAddress;
+	}
+
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+
+	public double getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empAddress=" + empAddress + ", empSalary="
+				+ empSalary + "]";
+	}
 	
 	
 
